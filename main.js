c2cab3ee785dd4cba44bb556e5721a16f9c0be05
Adipro-tech <79182071+Adipro-tech@users.noreply.github.com>
2021-11-10 20:52:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 song = "";
 
+rightWristX = 0;
+  rightWristY = 0;
+
+  leftWristX = 0;
+  leftWristY = 0;
+
 function setup()
 {
     canvas = createCanvas(600,500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
     image(video, 0,0, 600,500);
+    fill("#00ffcc");
+    stroke("#134239");
+    circle(leftWristX, leftWristY, 20);
+    numY = Number(leftWristY);
+    remove_decimal = floor(numY);
+    volume = remove_decimal/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);    
 }
 
 function preload()
@@ -21,9 +37,32 @@ function preload()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function stop()
 {
     song.stop();
 }
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+    }
+}
+
